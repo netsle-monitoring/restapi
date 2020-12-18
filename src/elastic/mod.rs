2a85cb0c6bb1246eb5aa1c4f0b76230aec8b8483
amit <gamesil456@gmail.com>
2020-12-18 19:23:58 +0200

    Basic auth guard which works

---
+++ src/elastic/mod.rs
@@ -0,0 +1,3 @@
+pub mod client;
+
+pub struct ElasticClient(pub client::Client);
---
+++ src/routes/get.rs
@@ -0,0 +1,38 @@
+use crate::elastic;
+use rocket::State;
+use rocket::{
+    Outcome,
+    http::{Status},
+    response::{self, content},
+    request::{self, Request, FromRequest}
+};
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Hello, netsle!"
+}
+
+#[get("/netstats")]
+pub fn network_stats(elastic: State<elastic::ElasticClient>) -> content::Json<String> {
+    let response = elastic.0.match_all_for_index("netsle");
+    content::Json(response)
+}
+
+pub struct ApiKey(String);
+
+#[derive(Debug)]
+pub enum ApiKeyError {
+    Invalid
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+    type Error = ApiKeyError;
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid))
+    }
+}
+
+#[get("/home")]
+pub fn home(key: ApiKey) -> &'static str {
+    "Sensitive data."
+}
